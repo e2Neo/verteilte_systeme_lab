ebae14e7177095ee275d3eaf71db8ede5ef74cca
e2Neo <fabiansonek@live.deC:/Users/Anwender/anaconda3/Scripts/activate>
2024-12-13 00:18:30 +0100

    added success message for deleting an item

--- frontend/src/clients/statusCodeHandler.js
+++ frontend/src/clients/statusCodeHandler.js
@@ -1,29 +1,32 @@
 import Toast from "primevue/toast";
 import { ToastService } from "../services/ToastService";
 
-// Can be used to define custom messages for specific status codes 
+// Can be used to define custom messages for specific status codes
 export function handleResponse(response) {
   console.log(response);
   switch (response.status) {
     // Can be extended here with handlers for specific or common status codes
     case 201:
       ToastService.success("Item created successfully.");
+      break;
+    case 204:
+      ToastService.success("Item deleted successfully.");
+      break;
     default:
       break;
   }
-  return response
+  return response;
 }
 // Can be used to define custom messages for specific error codes
 export function handleError(error) {
   console.error(error);
   switch (error.response.status) {
-    case 404: 
-      ToastService.error("Item not found.")
+    case 404:
+      ToastService.error("Item not found.");
+      break;
     default:
       ToastService.error(error.status + ": " + error.response.statusText);
       break;
   }
   return Promise.reject(error);
 }
-
-
