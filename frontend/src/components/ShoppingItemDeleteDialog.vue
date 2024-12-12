a2085549078ef52af0c90ff0e933c641936859b7
e2Neo <fabiansonek@live.deC:/Users/Anwender/anaconda3/Scripts/activate>
2024-12-13 00:47:55 +0100

    implemented functionality for delete button

--- frontend/src/components/ShoppingItemDeleteDialog.vue
+++ frontend/src/components/ShoppingItemDeleteDialog.vue
@@ -1,22 +1,31 @@
 <template>
-  <Dialog :visible="props.visible" :closable="false">
+  <Dialog :visible="props.visible" :closable="false" modal>
     <template #header><h1>Delete Item</h1></template>
     Are you sure you want to delete {{ itemToDelete.amount }} of item "{{
       itemToDelete.name
     }}"?
-    <template #footer><Button label="Cancel" @click="props.hideDeleteDialog"/><Button label="Delete permanently"/></template>
+    <template #footer>
+      <Button label="Cancel" @click="props.hideDeleteDialog" />
+      <Button label="Delete permanently" @click="deleteItem" severity="danger" />
+    </template>
   </Dialog>
 </template>
 
 <script setup>
 import { ref, defineProps } from "vue";
+import { ShoppingClient } from "../clients/shoppingClient";
 
-let props = defineProps({
+const props = defineProps({
   visible: Boolean,
   itemToDelete: Object,
   updateTable: Function,
-  hideDeleteDialog: Function
+  hideDeleteDialog: Function,
 });
+
+async function deleteItem() {
+  const response = await ShoppingClient.deleteByName(props.itemToDelete?.name);
+  props.hideDeleteDialog();
+}
 </script>
 
 <style scoped></style>
