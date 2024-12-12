ca742e4710e470c774198490bdb339fdba65a6bd
e2Neo <fabiansonek@live.deC:/Users/Anwender/anaconda3/Scripts/activate>
2024-12-12 19:03:00 +0100

    added Home page with hello endpoints

--- frontend/src/views/Hello.vue
+++ frontend/src/views/Hello.vue
@@ -1,11 +1,36 @@
 <template>
-    Hello World
+  <Button label="To Shopping" as="router-link" to="/shopping"></Button>
+  <Card v-if="serverResponse" style="margin-bottom: 1rem">
+    <template #header>
+      <h1>Server says:</h1>
+    </template>
+    <template #content>
+      {{ serverResponse }}
+    </template>
+  </Card>
+  <Card>
+    <template #content>
+      <InputText v-model="name" placeholder="Enter your Name"></InputText>
+      <Button :disabled="!name" @click="sayHello()">Say hello!</Button>
+    </template>
+  </Card>
 </template>
 
 <script setup>
+import { ref, onBeforeMount } from "vue";
+import { HelloClient } from "@/clients/helloClient";
 
+const name = ref("");
+const serverResponse = ref("");
+onBeforeMount(async () => {
+  const response = await HelloClient.hello();
+  serverResponse.value = response.data;
+});
+async function sayHello() {
+  const response = await HelloClient.helloName(name.value);
+  serverResponse.value = response.data;
+  name.value = "";
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
