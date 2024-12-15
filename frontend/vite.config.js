c74408b35c0e39bb73eb62aa7e6c0defb8901ac1
e2Neo <fabiansonek@live.deC:/Users/Anwender/anaconda3/Scripts/activate>
2024-12-15 19:15:15 +0100

    added entrypoint to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,19 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      // Ensure these entry points are explicitly set
+      input: {
+        client: "/src/entry-client.js", // Client entry point
+        server: "/src/entry-server.js", // Server entry point
+      },
+    },
+    ssr: {
+      // Define the SSR entry explicitly
+      input: "/src/entry-server.js",
+    },
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -30,8 +43,8 @@ export default defineConfig({
       "/backend": {
         target: "http://backendspringboot:8080/",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/backend/, "")
-      }
+        rewrite: (path) => path.replace(/^\/backend/, ""),
+      },
     },
   },
 });
